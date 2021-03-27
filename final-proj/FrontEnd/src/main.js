c6046ab37a940860dd4facd515ce8a3ac498094f
philosoph10 <77683432+philosoph10@users.noreply.github.com>
2021-03-27 22:17:57 +0200

    Home page
    Home page created

--- final-proj/FrontEnd/src/main.js
+++ final-proj/FrontEnd/src/main.js
@@ -1,6 +1,3 @@
 $(function () {
-    // To check that file builds into the right directory
-    $('.btn-primary').on('click', function () {
-        alert("Button clicked!");
-    });
+    let homePage = require('./mainPage/home');
 });
---
+++ final-proj/FrontEnd/src/mainPage/home.js
@@ -0,0 +1,3 @@
+$('#brand').on('click', function () {
+    window.location.href = 'http://localhost:3989';
+})
