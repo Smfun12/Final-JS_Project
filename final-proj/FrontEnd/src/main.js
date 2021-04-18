6b815a9117e34661f869f1fc040293f10948478b
Sasha <sasha@gmail.com>
2021-04-18 20:33:11 +0300

    Refactored code

--- final-proj/FrontEnd/src/main.js
+++ final-proj/FrontEnd/src/main.js
@@ -2,4 +2,6 @@ $(function () {
     let homePage = require('./mainPage/home');
     let signUpPage = require('./signUp/forSignUp');
     let archivePage = require('./viewDeliveries/archive');
+    let loginPage = require('./login/login');
+    let profilePage = require('./profile/profile');
 });
