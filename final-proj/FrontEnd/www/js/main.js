31fbe151726ba6c011b1fc98f4e6b1741a9425df
dieumerci7 <77683495+dieumerci7@users.noreply.github.com>
2021-04-26 11:16:59 +0300

    Date: 2465

--- final-proj/FrontEnd/www/js/main.js
+++ final-proj/FrontEnd/www/js/main.js
@@ -358,7 +358,7 @@ let orderData = "";
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+var yyyy = today.getFullYear() + 444;
 today = yyyy + '-' + mm + '-' + dd;
 
 let payer = "Sender";
